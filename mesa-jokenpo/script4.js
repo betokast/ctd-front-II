6a431c618a8c78c4a4197a419703d4deb74f8029
Roberto <bkastro@gmail.com>
2022-03-03 10:47:04 -0300

    3 - atualização do script, utilizando a estrutura if

--- mesa-jokenpo/script4.js
+++ mesa-jokenpo/script4.js
@@ -2,67 +2,83 @@
 // 2 - Papel
 // 3 - Tesoura
 
-for (let i = 0; i < 2; i++) {
+let partida = 0;
+let pontosUsuario = 0;
+let pontosComputador = 0;
+
+while (partida < 10) {
+  partida++;
   var palpiteUsuario = prompt("Digite 1 (Pedra), 2 (Papel) ou 3 (Tesoura)");
   var palpiteConvertido = parseInt(palpiteUsuario);
 
   var palpiteComputador = Math.round(Math.random() * (3 - 1) + 1);
   palpiteComputadorConv = parseInt(palpiteComputador);
 
-  // var partida = 0;
-  var pontosUsuario = 0;
-  var pontosComputador = 0;
-
-  switch (palpiteConvertido) {
-    // Usuario Pedra (1)
-    case palpiteConvertido == 1 && palpiteComputadorConv == 1:
-      console.log("Empate");
-      break;
-
-    case palpiteConvertido == 1 && palpiteComputadorConv == 2:
-      pontosComputador++;
-      console.log("Ponto computador");
-      break;
-
-    case palpiteConvertido == 1 && palpiteComputadorConv == 3:
-      pontosUsuario++;
-      console.log("Ponto usuário");
-      break;
-
-    // Usuario Papel (2)
-    case palpiteConvertido == 2 && palpiteComputadorConv == 2:
-      console.log("Empate");
-      break;
-
-    case palpiteConvertido == 2 && palpiteComputadorConv == 1:
-      pontosUsuario++;
-      console.log("Ponto usuário");
-      break;
-
-    case palpiteConvertido == 2 && palpiteComputadorConv == 3:
-      pontosComputador++;
-      console.log("Ponto computador");
-      break;
-
-    // Usuario Tesoura (3)
-    case palpiteConvertido == 3 && palpiteComputadorConv == 3:
-      console.log("Empate");
-      break;
-
-    case palpiteConvertido == 3 && palpiteComputadorConv == 1:
-      pontosComputador++;
-      console.log("Ponto computador");
-      break;
-
-    case palpiteConvertido == 3 && palpiteComputadorConv == 2:
-      pontosUsuario++;
-      console.log("Ponto usuário");
-      break;
+  if (palpiteConvertido == 1) {
+    if (palpiteComputadorConv == 1) {
+      console.log(
+        `O usuário jogou ${palpiteConvertido} e o computador jogou ${palpiteComputadorConv}. Empate`
+      );
+    } else if (palpiteComputadorConv == 2) {
+      pontosComputador += 1;
+      console.log(`Pontos computador: ${pontosComputador}`);
+      console.log(
+        `O computador jogou ${palpiteComputadorConv}. Ponto computador`
+      );
+    } else if (palpiteComputadorConv == 3) {
+      pontosUsuario += 1;
+      console.log(`Pontos usuário: ${pontosUsuario}`);
+      console.log(`O usuário jogou ${palpiteConvertido}. Ponto usuário`);
+    }
+    if (pontosComputador == 2) {
+      console.log("O computador venceu!");
+    } else if (pontosUsuario == 2) {
+      console.log("O Usuário venceu");
+    }
+  }
+  if (palpiteConvertido == 2) {
+    if (palpiteComputadorConv == 2) {
+      console.log(
+        `O usuário jogou ${palpiteConvertido} e o computador jogou ${palpiteComputadorConv}. Empate`
+      );
+    } else if (palpiteComputadorConv == 1) {
+      pontosComputador += 1;
+      console.log(`Pontos computador: ${pontosComputador}`);
+      console.log(
+        `O computador jogou ${palpiteComputadorConv}. Ponto computador`
+      );
+    } else if (palpiteComputadorConv == 3) {
+      pontosUsuario += 1;
+      console.log(`Pontos usuário: ${pontosUsuario}`);
+      console.log(`O usuário jogou ${palpiteConvertido}. Ponto usuário`);
+    }
+    if (pontosComputador == 2) {
+      console.log("O computador venceu!");
+    } else if (pontosUsuario == 2) {
+      console.log("O Usuário venceu");
+    }
   }
 
-  if (pontosComputador == 2) {
-    console.log("O computador venceu!");
-  } else if (pontosUsuario == 2) {
-    console.log("O Usuário venceu");
+  if (palpiteConvertido == 3) {
+    if (palpiteComputadorConv == 3) {
+      console.log(
+        `O usuário jogou ${palpiteConvertido} e o computador jogou ${palpiteComputadorConv}. Empate`
+      );
+    } else if (palpiteComputadorConv == 1) {
+      pontosComputador += 1;
+      console.log(`Pontos computador: ${pontosComputador}`);
+      console.log(
+        `O computador jogou ${palpiteComputadorConv}. Ponto computador`
+      );
+    } else if (palpiteComputadorConv == 2) {
+      pontosUsuario += 1;
+      console.log(`Pontos usuário: ${pontosUsuario}`);
+      console.log(`O usuário jogou ${palpiteConvertido}. Ponto usuário`);
+    }
+    if (pontosComputador == 2) {
+      console.log("O computador venceu!");
+    } else if (pontosUsuario == 2) {
+      console.log("O Usuário venceu");
+    }
   }
 }
