6f8bbd9266463d2a9af0a0396cdddfe83f438297
Roberto <bkastro@gmail.com>
2022-03-04 10:59:37 -0300

    Ajustes finais no js

--- mesa-jokenpo/script.js
+++ mesa-jokenpo/script.js
@@ -19,60 +19,77 @@ while (partida) {
   var palpiteComputador = Math.round(Math.random() * (3 - 1) + 1);
   palpiteComputadorConv = parseInt(palpiteComputador);
 
-  if (palpiteUserConvertido == 1) {
-    //1 - Pedra
-    if (palpiteComputadorConv == 1) {
-      console.log(`O usuário jogou pedra e o computador também. Empate`);
-    } else if (palpiteComputadorConv == 2) {
-      pontosComputador += 1;
-      console.log(`Pontos computador: ${pontosComputador}`);
-      console.log(
-        `Usuário jogou pedra e o computador jogou papel. Ponto computador`
-      );
-    } else if (palpiteComputadorConv == 3) {
-      pontosUsuario += 1;
-      console.log(`Pontos usuário: ${pontosUsuario}`);
-      console.log(
-        `O usuário jogou pedra e o computador jogou tesoura. Ponto usuário`
-      );
-    }
-  }
-  if (palpiteUserConvertido == 2) {
-    //Papel
-    if (palpiteComputadorConv == 2) {
-      console.log(`O usuário jogou papel e o computador também. Empate`);
-    } else if (palpiteComputadorConv == 1) {
-      pontosComputador += 1;
-      console.log(`Pontos computador: ${pontosComputador}`);
-      console.log(
-        `O usuário jogou papel e o computador jogou pedra. Ponto usuário`
-      );
-    } else if (palpiteComputadorConv == 3) {
-      pontosUsuario += 1;
-      console.log(`Pontos usuário: ${pontosUsuario}`);
-      `O usuário jogou papel e o computador jogou pedra. Ponto computador`;
-    }
+  switch (palpiteUserConvertido) {
+    case 1: //I - Caso o usuário digite 1 (Pedra):
+      if (palpiteComputadorConv == 1) {
+        // Caso haja igualdade de palpites, retorna o empate da partida. Ninguém pontua.
+        console.log(`O usuário jogou pedra e o computador também. Empate`);
+      } else if (palpiteComputadorConv == 2) {
+        //Caso o usuário digite 1 (pedra) e o computador escolha 2 (papel), o computador pontua.
+        pontosComputador += 1;
+        console.log(`Pontos computador: ${pontosComputador}`);
+        console.log(
+          `Usuário jogou pedra e o computador jogou papel. Ponto computador`
+        );
+      } else if (palpiteComputadorConv == 3) {
+        //Caso o usuário digite 1 (pedra) e o computador escolha 3 (Tesoura), o usuário pontua.
+        pontosUsuario += 1;
+        console.log(`Pontos usuário: ${pontosUsuario}`);
+        console.log(
+          `O usuário jogou pedra e o computador jogou tesoura. Ponto usuário`
+        );
+      }
+      break;
+
+    case 2: // II - Caso o usuário digite 2 (Papel):
+      if (palpiteUserConvertido == 2) {
+        if (palpiteComputadorConv == 2) {
+          // Caso haja igualdade de palpites, retorna o empate da partida. Ninguém pontua.
+          console.log(`O usuário jogou papel e o computador também. Empate`);
+        } else if (palpiteComputadorConv == 1) {
+          //Caso o usuário digite 2 (papel) e o computador escolha 1 (Pedra), o usuário pontua.
+          pontosUsuario += 1;
+          console.log(`Pontos usuário: ${pontosUsuario}`);
+          console.log(
+            `O usuário jogou papel e o computador jogou pedra. Ponto usuário`
+          );
+        } else if (palpiteComputadorConv == 3) {
+          //Caso o usuário digite 2 (papel) e o computador escolha 3 (Tesoura), o computador pontua.
+          pontosComputador += 1;
+          console.log(`Pontos computador: ${pontosComputador}`);
+          console.log(
+            `O usuário jogou papel e o computador jogou tesoura. Ponto computador`
+          );
+        }
+      }
+      break;
+
+    case 3: // III - Caso o usuário digite 3 (Tesoura)
+      if (palpiteUserConvertido == 3) {
+        if (palpiteComputadorConv == 3) {
+          // Caso haja igualdade de palpites, retorna o empate da partida. Ninguém pontua.
+          console.log(`O usuário jogou tesoura e o computador também. Empate`);
+        } else if (palpiteComputadorConv == 1) {
+          //Caso o usuário digite 3 (tesoura) e o computador escolha 1 (Pedra), o computador pontua.
+          pontosComputador += 1;
+          console.log(`Pontos computador: ${pontosComputador}`);
+          console.log(
+            `O usuário jogou tesoura e o computador jogou pedra. Ponto computador`
+          );
+        } else if (palpiteComputadorConv == 2) {
+          //Caso o usuário digite 3 (tesoura) e o computador escolha 2 (papel), o usuário pontua.
+          pontosUsuario += 1;
+          console.log(`Pontos usuário: ${pontosUsuario}`);
+          console.log(
+            `O usuário jogou tesoura e o computador jogou papel. Ponto usuário`
+          );
+        }
+      }
+      break;
   }
 
-  if (palpiteUserConvertido == 3) {
-    //Tesoura
-    if (palpiteComputadorConv == 3) {
-      console.log(`O usuário jogou tesoura e o computador também. Empate`);
-    } else if (palpiteComputadorConv == 1) {
-      pontosComputador += 1;
-      console.log(`Pontos computador: ${pontosComputador}`);
-      console.log(
-        `O usuário jogou tesoura e o computador jogou pedra. Ponto computador`
-      );
-    } else if (palpiteComputadorConv == 2) {
-      pontosUsuario += 1;
-      console.log(`Pontos usuário: ${pontosUsuario}`);
-      console.log(
-        `O usuário jogou tesoura e o computador jogou papel. Ponto usuário`
-      );
-    }
-  }
-
+  //Validação da pontuação. Caso um dos dois atinja essa pontuação, o while termina,
+  //e é declarado vencedor o que atingiu os 2 pontos primeiros
   if (pontosComputador == 2) {
     console.log("O computador venceu!");
     partida = false;
